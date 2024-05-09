e0e71e851ab05c334dcd6fd22255fd8c2100cd3c
Matikilkil <jerotichlydiah95@gmail.com>
2024-05-08 23:46:49 -0700

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,8 +4,9 @@ log_file = ARGV[0]
 
 log_data = File.read(log_file)
 
-regex_pattern = /\[from:(?<sender>.*?)\] \[to:(?<receiver>.*?)\] \[flags:(?<flags>.*?)\]/
+regex_pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
 
 log_data.scan(regex_pattern) do |sender, receiver, flags|
+
   puts "#{sender},#{receiver},#{flags}"
 end
